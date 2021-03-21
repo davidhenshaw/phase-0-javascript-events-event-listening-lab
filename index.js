df0347a3697f67f220fbc8bda3f2a2bae7073c01
davidhenshaw <davidhenshaw2014@gmail.com>
2021-03-21 10:10:12 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
+function clickAlert()
+{
+    alert('I was clicked');
 }
+
+function addingEventListener() {
+    const input = document.getElementById('input');
+    const secondDiv = document.querySelector('div ~ div');
+
+    input.addEventListener('click', clickAlert); 
+    secondDiv.addEventListener('click', clickAlert);
+}
+
+addingEventListener();
